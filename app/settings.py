18a395025df97dca2ea70a6a01e5c7b8ddba74d9
albanidcl <alban@dcodinglabs.com>
2024-02-03 23:55:37 +0100

    Connected to a local Postgres db

--- app/settings.py
+++ app/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recruter_plus',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
