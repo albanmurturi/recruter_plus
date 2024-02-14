97601105b6d86658c13abba439ef47bcb9cf8c59
albanidcl <alban@dcodinglabs.com>
2024-02-14 15:38:15 +0100

    Adjusted interwiev detail to return a random question

--- interview/views.py
+++ interview/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Prefetch
+from django.db.models import F
 
 from interview.models import Interview
-from django.db.models import Prefetch
+from subject.models import Question
+
 
 # Create your views here.
 @login_required
 def interview(request):
-    interviews = Interview.objects.all()
+    user = request.user
+    interviews = Interview.objects.filter(user=user)
     print("interviews", interviews)
     return render(request, 'interview/interview.html', {
         'interviews': interviews
@@ -15,17 +20,27 @@ def interview(request):
     
 @login_required
 def interview_detail(request, id):
-    # Fetch the interview by id and render a detail template
-    interview = get_object_or_404(Interview, id=id)
 
-    # Prefetch subjects and their related questions
-    subjects_with_questions = interview.subjects.prefetch_related(
-        Prefetch('questions')
-    ).all()
+    user = request.user
+    interviews = Interview.objects.filter(user=user)
+
+    # Fetch the interview by id and render a detail template
+    interview = get_object_or_404(interviews, id=id)
+
+    # Annotate a random question with its subject
+    random_question = Question.objects.filter(subject__in=interview.subjects.all()).annotate(
+        subject__id=F('subject_id'),
+        subject_name=F('subject__name'),
+        subject_description=F('subject__description'),
+        question_id=F('id'),
+        question_text=F('text'),
+        question_answer=F('answer'),
+    ).order_by('?').first()
+
 
     # Prepare the context for rendering
     context = {
         'interview': interview,
-        'subjects': subjects_with_questions,
+        'subject_with_random_question': random_question,
     }
     return render(request, 'interview/interview_detail.html', context)
