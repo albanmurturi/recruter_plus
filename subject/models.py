9bea2b7e9b9d7ffb8406229e50f931d54d2dcb6d
albertrestelica <albert.restelicaa@gmail.com>
2024-02-16 11:42:57 +0100

    level to questions added

--- subject/models.py
+++ subject/models.py
@@ -10,11 +10,18 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
-
+    
 class Question(models.Model):
+    LEVEL_CHOICES = [
+        (1, 'FOUNDATIONAL'),
+        (2, 'PROFICIENT'),
+        (3, 'EXPERT'),
+    ]
     subject = models.ForeignKey(Subject, related_name="questions", on_delete=models.CASCADE)
     text = models.TextField()
     answer = models.TextField()
+    level = models.IntegerField(choices=LEVEL_CHOICES, default=1)
+    
     class Meta:
         ordering = ('id',)
 
